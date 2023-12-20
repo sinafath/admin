7da5edd6dac8c8f7d07f756362509a1a14349e44
sinafath <sina.fathpoor@gmail.com>
2023-12-20 22:32:53 +0330

    add delete cookie

--- libs/cookies/cookies.tsx
+++ libs/cookies/cookies.tsx
@@ -1,5 +1,7 @@
 import { cookies } from "next/headers"
 
 const getAccessToken = () => cookies().get('access_token')
+const deleteAccessToken = () => cookies().delete('access_token')
 
+export {deleteAccessToken}
 export default getAccessToken
