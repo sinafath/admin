356fd9f436d448cd2f0cf0fc27bf853bdac31a0a
sinafath <sina.fathpoor@gmail.com>
2023-12-21 18:33:07 +0330

    refactor dashboard ui files

--- app/@dashboard/_ui/Footer/Footer.tsx
+++ app/@dashboard/_ui/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { IconLogout } from '@tabler/icons-react';
 import classes from './Footer.module.css';
 import { useFormState, useFormStatus } from 'react-dom';
 import { IconLoader } from '@tabler/icons-react';
-import { logout } from '@/app/@dashboard/(products)/libs/actions';
+import { logout } from '@/app/@dashboard/(products)/_libs/actions';
 
 function LogoutButton() {
     const { pending } = useFormStatus()
