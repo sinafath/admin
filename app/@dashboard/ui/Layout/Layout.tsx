2b612c5856be63690d6866439775064c932a130f
sinafath <sina.fathpoor@gmail.com>
2023-12-21 14:01:08 +0330

    add header

--- app/@dashboard/ui/Layout/Layout.tsx
+++ app/@dashboard/ui/Layout/Layout.tsx
@@ -1,16 +1,28 @@
 "use client"
-import { AppShell, ScrollArea } from '@mantine/core';
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
 import NavbarNested from './NavbarNested/NavbarNested';
 import { ReactNode } from 'react';
+import { IconDog } from '@tabler/icons-react';
+import { useDisclosure } from '@mantine/hooks';
 
 type LayoutProps = {
     children: ReactNode
 }
 function Layout({ children }: LayoutProps) {
+    const [opened, { toggle }] = useDisclosure();
+
     return (
-        <AppShell navbar={{ width: 300, breakpoint: 300 }}>
+        <AppShell header={{ height: 60 }}
+            navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                    <IconDog size={30} />
+                </Group>
+            </AppShell.Header>
             <NavbarNested />
-            <AppShell.Main m={70}>{children}</AppShell.Main>
+            <AppShell.Main m={70} my={0} pt={80}>{children}</AppShell.Main>
         </AppShell>
     );
 }
