5104dcc3d717b9cb4919cedb2530fd35df9c9a5e
sinafath <sina.fathpoor@gmail.com>
2023-12-26 19:12:38 +0330

    fix import

--- app/@dashboard/(products)/_libs/actions.tsx
+++ app/@dashboard/(products)/_libs/actions.tsx
@@ -2,7 +2,7 @@
 import { revalidatePath } from 'next/cache';
 import { cookies } from 'next/headers'
 import { stateLogout } from './types';
-import { deleteAccessToken } from '@/libs/http/cookies/cookies';;
+import { deleteAccessToken } from '@/libs/http/cookies/accessToken';;
 
 export async function logout(currentState: stateLogout): Promise<stateLogout> {
     deleteAccessToken()
