2af8897523b7c9a614abfd3f786977d395f83a2c
sinafath <sina.fathpoor@gmail.com>
2023-12-27 14:29:37 +0330

    correct login components with new ones

--- app/@login/default.tsx
+++ app/@login/default.tsx
@@ -2,47 +2,30 @@
 import {
     Paper,
     TextInput,
-    PasswordInput,
-    Checkbox,
-    Button,
     Title,
-    Text,
-    Anchor,
 } from '@mantine/core';
 import classes from './Login.module.css';
-import { login } from './libs/actions';
-import { useFormState, useFormStatus } from 'react-dom';
-import { useEffect } from 'react';
-import { notifications } from '@mantine/notifications';
+import { login } from '../_libs/auth/actions';
+import Button from '../_components/Form/Submit';
+import PasswordInput from '../_components/Form/PasswordInput';
+import { Form } from '../_components/Form/Form';
 
-function SubmitButton() {
-    const { pending } = useFormStatus()
-    return (
-        <Button fullWidth mt="xl" size="md" type='submit' loading={pending}>
-            ورود
-        </Button>
-    )
-}
+
 
 function AuthenticationImage() {
-    const [state, dispatch] = useFormState(login, null)
-    useEffect(() => {
-        state?.statusCode === 401 && state?.statusCode === 401 && notifications.show({
-            title: state?.message,
-            message: state?.message,
-        })
-    }, [state])
     return (
         <div className={classes.wrapper}>
             <Paper className={classes.form} radius={0} p={30}>
                 <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
                     به پنل ادمین خوش آمدید
                 </Title>
-                <form action={dispatch}>
-                    <TextInput name='email' label="ایمیل" error={state?.errors?.email} placeholder="hello@gmail.com" size="md" />
-                    <PasswordInput name='password' label="رمز عبور" error={state?.errors?.password} placeholder="رمز عبور" mt="md" size="md" />
-                    <SubmitButton />
-                </form >
+                <Form action={login}>
+                    <TextInput label="ایمیل" placeholder="hello@gmail.com" size="md" />
+                    <PasswordInput />
+                    <Button>
+                        ورود
+                    </Button>
+                </Form >
             </Paper>
         </div>
     );
