78423f3cbc853a2e2b7564612c05ce48d05ed982
sinafth <sina.fathpoor@gmail.com>
2024-01-04 07:16:51 +0330

    add vars

--- playwright/config.ts
+++ playwright/config.ts
@@ -1,2 +1,5 @@
-const baseUrl = process.env.TEST_URL ?? "        "
+const baseUrl = process.env.TEST_URL ?? ""
+const email = process.env.EMAIL_AUTH ?? ""
+const password = process.env.PASSWORD_AUTH ?? ""
+export { email, password }
 export default baseUrl
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,13 +1,13 @@
 import { test as setup, expect } from '@playwright/test';
-import baseUrl from '../playwright/config';
+import baseUrl, { email, password } from '../playwright/config';
 
 const authFile = 'playwright/.auth/user.json';
 
 setup('authenticate', async ({ page }) => {
   await page.goto(baseUrl);
   await expect(page.getByText("به پنل ادمین خوش آمدید")).toBeVisible();
-  await page.getByLabel('ایمیل').fill('superAdmin@gmail.com');
-  await page.getByLabel('رمز عبور').fill('123');
+  await page.getByLabel('ایمیل').fill(email);
+  await page.getByLabel('رمز عبور').fill(password);
   await page.getByRole('button', { name: 'ورود' }).click();
   await expect(page.getByText('محصولات')).toBeVisible();
   await page.context().storageState({ path: authFile });
